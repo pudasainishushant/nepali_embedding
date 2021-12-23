eb19251d9edd96508ce1244027c37f3148819408
pudasainishushant <jit.khamcha@infodev.tech>
2021-12-23 16:17:58 +0545

    finalized sabda2vec and nepbert

--- nepBERT/embedding_generator.py
+++ nepBERT/embedding_generator.py
@@ -1,121 +1,84 @@
 import torch
-
-from transformers import BertTokenizer, BertForMaskedLM
 from scipy.spatial.distance import cosine 
 
-import os 
 
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 
-vocab_file = 'bert_custom_tokenizer/vocab.txt'
-
-tokenizer = BertTokenizer.from_pretrained(vocab_file)
-
-
-
-
-mlm_model = '/home/info/Aakash/bert custom/training_bert/checkpoint-530000'
-x = BertForMaskedLM.from_pretrained(pretrained_model_name_or_path=mlm_model,output_hidden_states=True)
-
-
-from transformers import pipeline
-
-feature_extraction = pipeline('feature-extraction', model=mlm_model, tokenizer=tokenizer)
-features = feature_extraction(["आकाश भाई ज्ञानी मन्छे हो",
-                               "त्यो समान एक्दमै नराम्रो रहेछ",
-                               "एस्तो खत्तम चलचित्र्‍ मैले आहिले सम्म हेरेको थिन"])
-
-
-# import pdb;pdb.set_trace()
-cos_dist_same = 1 - cosine(torch.mean(torch.Tensor(features[0][0]), dim=0),torch.mean(torch.Tensor(features[1][0]), dim=0))
-cos_dist_different = 1 - cosine(torch.mean(torch.Tensor(features[1][0]), dim=0),torch.mean(torch.Tensor(features[2][0]), dim=0))
-print("Difference",cos_dist_different)
-print("Similar",cos_dist_same)
-
-"""
-# from transformers import pipeline
-
-# fill_mask = pipeline(
-#     "fill-mask",
-#     model=x,
-#     tokenizer=tokenizer
-# )
-
-
-# MASK_TOKEN = tokenizer.mask_token
-
-# print(fill_mask("कांग्रेसले चुनाबमा एमालेसंग भारि मातले {} भेहोर्यो".format(MASK_TOKEN)))
-
-
-
-def get_word_embedding_bert(input_text,tokenizer,model):
-    marked_text = " [CLS] " + input_text + " [SEP] "
-    tokenized_text = tokenizer.tokenize(marked_text)
-    for i, token_str in enumerate(tokenized_text):
-      print (i, token_str)
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    segments_ids = [1] * len(indexed_tokens) 
+class NepBERT():
+    def __init__(self) -> None:
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained("Shushant/NepNewsBERT")
+        self.model = AutoModelForMaskedLM.from_pretrained("Shushant/NepNewsBERT",output_hidden_states=True)
     
-    # Convert inputs to Pytorch tensors
-    tokens_tensors = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
-    
-    with torch.no_grad():
-        outputs = model(tokens_tensors, segments_tensors)
-        # removing the first hidden state
-        # the first state is the input state 
-        hidden_states = outputs.hidden_states
-    
-    token_embeddings = torch.stack(hidden_states, dim=0)
-    token_embeddings = torch.squeeze(token_embeddings, dim=1)
-    token_embeddings = token_embeddings.permute(1,0,2)
-
-
-    # Stores the token vectors, with shape [22 x 768]
-    token_vecs_sum = []
-
-    # `token_embeddings` is a [22 x 12 x 768] tensor.
-
-    # For each token in the sentence...
-    for token in token_embeddings:
-
-        # `token` is a [12 x 768] tensor
-
-        # Sum the vectors from the last four layers.
-        sum_vec = torch.sum(token[-4:], dim=0)
+    def get_word_embedding_bert(self,input_text):
+        marked_text = " [CLS] " + input_text + " [SEP] "
+        tokenized_text = self.tokenizer.tokenize(marked_text)
+        for i, token_str in enumerate(tokenized_text):
+            print (i, token_str)
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+        segments_ids = [1] * len(indexed_tokens) 
         
-        # Use `sum_vec` to represent `token`.
-        token_vecs_sum.append(sum_vec)
-    return token_vecs_sum
+        # Convert inputs to Pytorch tensors
+        tokens_tensors = torch.tensor([indexed_tokens])
+        segments_tensors = torch.tensor([segments_ids])
+        
+        with torch.no_grad():
+            outputs = self.model(tokens_tensors, segments_tensors)
+            # removing the first hidden state
+            # the first state is the input state 
+            hidden_states = outputs.hidden_states
+        
+        token_embeddings = torch.stack(hidden_states, dim=0)
+        token_embeddings = torch.squeeze(token_embeddings, dim=1)
+        token_embeddings = token_embeddings.permute(1,0,2)
 
-def get_bert_embedding_sentence(input_sentence,tokenizer,model):
 
-    marked_text = " [CLS] " + input_sentence + " [SEP] "
-    tokenized_text = tokenizer.tokenize(marked_text)
-    for i, token_str in enumerate(tokenized_text):
-      print (i, token_str)
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    segments_ids = [1] * len(indexed_tokens) 
-    
-    # Convert inputs to Pytorch tensors
-    tokens_tensors = torch.tensor([indexed_tokens])
-    segments_tensors = torch.tensor([segments_ids])
-    
-    with torch.no_grad():
-        outputs = model(tokens_tensors, segments_tensors)
-        # removing the first hidden state
-        # the first state is the input state 
+        # Stores the token vectors, with shape [22 x 768]
+        token_vecs_sum = []
 
-        hidden_states = outputs.hidden_states
-        # second_hidden_states = outputs[2]
-    # `hidden_states` has shape [13 x 1 x 22 x 768]
+        # `token_embeddings` is a [22 x 12 x 768] tensor.
 
-    # import pdb;pdb.set_trace()
-    # `token_vecs` is a tensor with shape [22 x 768]
-    token_vecs = hidden_states[-2][0]
+        # For each token in the sentence...
+        for token in token_embeddings:
 
-    # Calculate the average of all 22 token vectors.
-    sentence_embedding = torch.mean(token_vecs, dim=0)
-    return sentence_embedding
+            # `token` is a [12 x 768] tensor
+
+            # Sum the vectors from the last four layers.
+            sum_vec = torch.sum(token[-4:], dim=0)
+            
+            # Use `sum_vec` to represent `token`.
+            token_vecs_sum.append(sum_vec)
+        return token_vecs_sum
+
+    def get_bert_embedding_sentence(self,input_sentence):
+
+        marked_text = " [CLS] " + input_sentence + " [SEP] "
+        tokenized_text = self.tokenizer.tokenize(marked_text)
+        for i, token_str in enumerate(tokenized_text):
+            print (i, token_str)
+        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
+        segments_ids = [1] * len(indexed_tokens) 
+        
+        # Convert inputs to Pytorch tensors
+        tokens_tensors = torch.tensor([indexed_tokens])
+        segments_tensors = torch.tensor([segments_ids])
+        
+        with torch.no_grad():
+            outputs = self.model(tokens_tensors, segments_tensors)
+            # removing the first hidden state
+            # the first state is the input state 
+            # import pdb;pdb.set_trace()
+            hidden_states = outputs.hidden_states
+            # second_hidden_states = outputs[2]
+        # `hidden_states` has shape [13 x 1 x 22 x 768]
+
+        # import pdb;pdb.set_trace()
+        # `token_vecs` is a tensor with shape [22 x 768]
+        token_vecs = hidden_states[-2][0]
+
+        # Calculate the average of all 22 token vectors.
+        sentence_embedding = torch.mean(token_vecs, dim=0)
+        return sentence_embedding
 
 
 if __name__== "__main__":
@@ -141,11 +104,11 @@ if __name__== "__main__":
     # print("Similar",cos_dist_same)
     # end = time.time()
     # print("Time",end-start)
-    
+    nepbert = NepBERT()
     #For sentence embedding
-    embedding1 = get_bert_embedding_sentence(test_sentence,tokenizer,x)
-    embedding2 = get_bert_embedding_sentence(test_sentence_2,tokenizer,x)
-    embedding3 = get_bert_embedding_sentence(test_sentence_3,tokenizer,x)
+    embedding1 = nepbert.get_bert_embedding_sentence(test_sentence,)
+    embedding2 = nepbert.get_bert_embedding_sentence(test_sentence_2)
+    embedding3 = nepbert.get_bert_embedding_sentence(test_sentence_3)
     # import pdb;pdb.set_trace()
     cos_dist_different = 1 - cosine(embedding1,embedding2)
     cos_dist_same = 1 - cosine(embedding2,embedding3)
@@ -153,4 +116,34 @@ if __name__== "__main__":
     print("Similar",cos_dist_same)
     end = time.time()
     print("Time",end-start)
-"""
+
+
+
+
+# from transformers import pipeline
+
+# feature_extraction = pipeline('feature-extraction', model=mlm_model, tokenizer=tokenizer)
+# features = feature_extraction(["आकाश भाई ज्ञानी मन्छे हो",
+#                                "त्यो समान एक्दमै नराम्रो रहेछ",
+#                                "एस्तो खत्तम चलचित्र्‍ मैले आहिले सम्म हेरेको थिन"])
+
+
+# # import pdb;pdb.set_trace()
+# cos_dist_same = 1 - cosine(torch.mean(torch.Tensor(features[0][0]), dim=0),torch.mean(torch.Tensor(features[1][0]), dim=0))
+# cos_dist_different = 1 - cosine(torch.mean(torch.Tensor(features[1][0]), dim=0),torch.mean(torch.Tensor(features[2][0]), dim=0))
+# print("Difference",cos_dist_different)
+# print("Similar",cos_dist_same)
+
+
+# from transformers import pipeline
+
+# fill_mask = pipeline(
+#     "fill-mask",
+#     model=x,
+#     tokenizer=tokenizer
+# )
+
+
+# MASK_TOKEN = tokenizer.mask_token
+
+# print(fill_mask("कांग्रेसले चुनाबमा एमालेसंग भारि मातले {} भेहोर्यो".format(MASK_TOKEN)))
