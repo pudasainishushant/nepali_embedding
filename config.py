5c917865339da636ac7add9e24ce9a4a48cc6543
Jitu Khamcha <lappanchappan43@gmail.com>
2021-12-27 16:21:41 +0545

    [Completed] download model

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ root_dir = os.path.expanduser('~/')
 
 
 '''<------------Word2Vec model path------------------------------>'''
-word2vec_model_sm_path = os.path.join(root_dir,'sabda_to_vec_model')
-word2vec_model_md_path = os.path.join(root_dir,"sabda_to_vec_model_md")
+word2vec_model_sm_path = os.path.join(root_dir,'everest_nlp_data', 'sabda_to_vec_model')
+word2vec_model_md_path = os.path.join(root_dir, 'everest_nlp_data', "sabda_to_vec_model_md")
 # nepali_fasttext_model = os.path.join(root_dir,"sabda2vec/models/","cc.ne.300.vec")
