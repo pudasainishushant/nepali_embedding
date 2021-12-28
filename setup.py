4cb8bb6d7efa78d4cc159fe93566cabfe4b98b05
Jitu Khamcha <lappanchappan43@gmail.com>
2021-12-28 15:58:39 +0545

    Include nepbert in package

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    packages=find_packages(),
     install_requires=[
         "transformers==4.15.0",
         "scipy",
